5feb05324e789dc10233d86e38bf98df7f9cb874
Wes Rupert <ora@outlook.com>
2013-03-28 13:19:28 -0400

    Added TowerRange.cs

---
+++ Assets/scripts/Towers/TowerRange.cs
@@ -0,0 +1,34 @@
+/// Wes Rupert - wkr3
+/// EECS 290   - Project 03
+/// Purgatory  - TowerRange.cs
+/// Script to control the range of a tower.
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// The range of a tower.
+/// </summary>
+public class TowerRange {
+    public Tower ParentTower;
+
+    void Start() {
+        transform.localScale = new Vector3(
+                ParentTower.Range,
+                ParentTower.Range,
+                1f);
+    }
+
+    void Update() {
+    
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        ParentTower.Targets.Add(collider.gameObject);
+    }
+
+    void OnTriggerExit(Collider collider) {
+        ParentTower.Targets.Remove(collider.gameObject);
+    }
+}
